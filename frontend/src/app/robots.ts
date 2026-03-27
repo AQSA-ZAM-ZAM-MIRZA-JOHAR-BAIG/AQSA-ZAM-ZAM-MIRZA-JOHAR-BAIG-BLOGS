b51761949f4e6c44e1e3e6c92caaf61be87dddc5
ayush8 <ayushdayal900@gmail.com>
2026-03-27 07:47:52 +0530

    fix robots for sitemap

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://aqsa-zam-zam-mirza-johar-baig.com';
+  const baseUrl = 'https://aqsa-zam-zam-mirza-johar-baig-blogs.vercel.app'
 
   return {
     rules: [
